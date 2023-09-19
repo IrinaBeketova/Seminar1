7ede2a12650049dc8a7a2bce8b214b28981f1a66
Renard <onlybags111@gmail.com>
2023-09-19 20:40:49 +0300

    Task_002_over

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -6,4 +6,14 @@
 // a = -3 b = 9 -> нет
 
 Console.WriteLine("Введите 1 число");
-
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите 2 число"); 
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+if (number1 == number2 * number2) 
+{ 
+    Console.WriteLine($"{number1} является квадратом {number2}"); 
+    } 
+    else 
+    { 
+        Console.WriteLine($"{number1} не является квадратом {number2}"); 
+        }
