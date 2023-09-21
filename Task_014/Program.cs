e942ea8e9a9c18f2611b8e1c38aa6911e004eb7c
Renard <onlybags111@gmail.com>
2023-09-21 21:58:06 +0300

    Task_014

---
+++ Task_014/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool MultipleOfTwoNumbers (int num, int num1, int num2)
+{
+    return num % 7 == 0 && num % 23 ==0;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.Toint32(Co)
