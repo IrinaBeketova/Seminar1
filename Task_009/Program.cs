01b4d88ab3d660b213f3dd37430f69bfa775d9e8
Renard <onlybags111@gmail.com>
2023-09-21 21:56:31 +0300

    Task_009-1

--- Task_009/Program.cs
+++ Task_009/Program.cs
@@ -5,3 +5,14 @@
 // 12-> 2
 // 85 -> 8
 
+int number = new Random().Next(10, 99 + 1); // max = 99+1
+Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
+
+int firstDigit = number / 10; // 78 / 10 = 7
+int secondDigit = number % 10;
+
+if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; // Тернарный оператор
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
