991e5e80f393a202e9e3cdf09c6e1ae8e3c53989
Renard <onlybags111@gmail.com>
2023-09-23 21:21:12 +0300

    Task_016_

--- Task_016/Program.cs
+++ Task_016/Program.cs
@@ -6,3 +6,13 @@
 // 25, 5 -> да
 // 8,9 -> нет
 
+bool IsOneSquareOfTheOther(int num1, int num2) 
+{ 
+return num1 * num1 == num2 || num2 * num2 == num1; 
+}
+Console.WriteLine($"ввидите первое число"); 
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine($"ввидите второе число"); 
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+bool result = IsOneSquareOfTheOther(number1, number2); 
+Console.WriteLine(result ? "да" : "нет");
