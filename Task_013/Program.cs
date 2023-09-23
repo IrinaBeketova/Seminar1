7948135d038b6744c286cfefd61a84e5b07b9c5a
Renard <onlybags111@gmail.com>
2023-09-23 22:25:51 +0300

    Task_013_

---
+++ Task_013/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Variable(string array)
+{
+    Console.Write(array);
+    string toKnow = Console.ReadLine();
+    int result = Convert.ToInt32(toKnow);
+    return result;
+}
+int thirdDigit(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+bool Examination(int number)
+{
+    if (number < 100)
+    {
+      Console.WriteLine("Третьей цифры нет");
+      return false;  
+    }
+    return true;
+}
+int number = Variable("Введите любое число > ");
+if (Examination(number))
+{
+    Console.WriteLine(thirdDigit(number));
+}
