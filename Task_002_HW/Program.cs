f10fd91b62d141bdc299e2ece2283e63d22062d7
Renard <onlybags111@gmail.com>
2023-09-23 12:50:14 +0300

    Task_002_HW - done

--- Task_002_HW/Program.cs
+++ Task_002_HW/Program.cs
@@ -3,3 +3,18 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"max = {firstNumber}");
+}
+else if (secondNumber > firstNumber)
+{
+    Console.WriteLine($"max = {secondNumber}");
+}
