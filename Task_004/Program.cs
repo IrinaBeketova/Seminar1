2fb69c7e4d9d98bc1b51f5bce3f95d0b11f745ae
Renard <onlybags111@gmail.com>
2023-09-23 13:55:15 +0300

    Task_004 - done

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -3,3 +3,33 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+    int max = firstNumber;
+}
+if (firstNumber > thirdNumber)
+{
+    int max = firstNumber;
+    Console.WriteLine($"-> {max}");
+}
+else if (firstNumber < secondNumber)
+{
+    int max = secondNumber;
+}
+if (secondNumber > thirdNumber)
+{
+    int max = secondNumber;
+    Console.WriteLine($"-> {max}");
+
+    if (max < thirdNumber)
+    {
+        Console.WriteLine($"-> {thirdNumber}");
+    }
+}
