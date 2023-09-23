6353d19e2b86c4717912a7b5713301effe21d830
Renard <onlybags111@gmail.com>
2023-09-23 14:36:40 +0300

    Task_006 - done

--- Task_006/Program.cs
+++ Task_006/Program.cs
@@ -3,3 +3,15 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите любое число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = Number % 2;
+if (lastDigit == 0)
+{
+    Console.WriteLine($"да");
+}
+else
+{
+    Console.WriteLine($"нет");
+}
