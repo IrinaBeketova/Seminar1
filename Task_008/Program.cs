993a78ad0a18d9cebbc97e703f91ca3fbaf3fe84
Renard <onlybags111@gmail.com>
2023-09-23 14:54:35 +0300

    Task_008 - done

--- Task_008/Program.cs
+++ Task_008/Program.cs
@@ -2,3 +2,13 @@
 // а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i < (number - 1))
+{
+    i += 2;
+    Console.WriteLine(i);
+}
