b86631b953d6bc197b6485671dadb0ce8f1af065
Renard <onlybags111@gmail.com>
2023-09-29 01:34:41 +0300

    Task_029

---
+++ Task_029/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+int[] CreateArray(int Length, int minVaiue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random rnd = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = rnd.Next(minVaiue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+System.Console.Write($"{array[array.Length - 1]}");
+System.Console.WriteLine("]");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение: ");
+int max = Prompt("Конечное значение: ");
+int[] arr = CreateArray(length, min, max);
+PrintArray(arr);
